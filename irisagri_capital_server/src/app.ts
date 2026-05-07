25f3671f12d6fc68c310d1df425c4d060f470292
Godwin <petergodwin437@icloud.com>
2026-05-07 12:25:44 +0300

    backend

--- irisagri_capital_server/src/app.ts
+++ irisagri_capital_server/src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend
+    origin: "*", 
     methods: ["GET", "POST"],
     credentials: true,
   })
